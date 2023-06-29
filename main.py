938a2224840cc425049b7fb69810a31a7e89d133
Irina Makarova <irisha-makarova_@mail.ru>
2023-06-29 23:21:50 +0300

    ycrawler download news to filesystem async

--- main.py
+++ main.py
@@ -1,68 +1,129 @@
+import asyncio
+import socket
+
+import aiofiles
+import aiohttp
+import logging
 import os
 import re
+import time
 import uuid
-
 import requests
 from bs4 import BeautifulSoup
 
 URL = "https://news.ycombinator.com"
+Y_LINK = 'ycombinator.com'
 DOWNLOAD_DIR = "/tmp/ycrawler"
-CYCLE_SECONDS = 120 #sec
+CYCLE_SECONDS = 10 #sec
+REQUEST_TIMEOUT = 5 #sec
 
 
-def create_dir(dir_name):
-    path = DOWNLOAD_DIR + dir_name
-    if not os.path.exists(path):
-        os.makedirs(path)
-
-
-def write_to_file(filename, content):
-    f = open(filename, "w")
-    f.write(content)
-    f.close()
+async def write_to_file(filename, content):
+    async with aiofiles.open(filename, "w") as f:
+        await f.write(content)
 
 
 def generate_name_from_link(url):
     return "/" + str(uuid.uuid4())
 
 
-def find_news_blocks(url):
+async def find_news_blocks(session, url):
     blocks = []
-    page = requests.get(url)
-    soup = BeautifulSoup(page.text, "html.parser")
-    news_headers = soup.findAll('tr', class_='athing')
-    for tr in news_headers:
-        comments_block = tr.findNextSibling('tr')
-        comment_link = comments_block.find('a', string=re.compile('comment(s|)$'))
-        news_link = tr.find('span', class_='titleline')
-        news_link = news_link.a if news_link else None
-        news_link = news_link['href'] if news_link else None
-        if news_link is not None and 'http' != news_link[:4]:
-            news_link = URL + '/' + news_link
+    try:
+        page = await session.get(url, timeout=REQUEST_TIMEOUT)
+        page = await page.text()
+        soup = BeautifulSoup(page, "html.parser")
+        news_headers = soup.findAll('tr', class_='athing')
+        for tr in news_headers:
+            comments_block = tr.findNextSibling('tr')
+            comment_link = comments_block.find('a', string=re.compile('comment(s|)$'))
+            news_link = tr.find('span', class_='titleline')
+            news_link = news_link.a if news_link else None
+            news_link = news_link['href'] if news_link else None
+            if news_link is not None and 'http' != news_link[:4]:
+                news_link = URL + '/' + news_link
 
-        block = {'news_link': news_link,
-                 'comment_link': (URL + '/' + comment_link['href']) if comment_link else None}
-        blocks.append(block)
-    return blocks
+            block = {'news_link': news_link,
+                     'comment_link': (URL + '/' + comment_link['href']) if comment_link else None}
+            blocks.append(block)
+        return blocks
+    except Exception as e:
+        return []
 
 
-def get_content(url):
-    response = requests.get(url)
-    content = response.content.decode('utf-8')
+async def get_content(session, url):
+    content = ''
+    try:
+        response = await session.get(url, timeout=REQUEST_TIMEOUT)
+        content = await response.text()
+    except Exception as e:
+        logging.warning("No response from {}".format(url))
     return content
 
 
-def main():
-    all_news = find_news_blocks(URL)
-    for news in all_news:
+def filter_link(link):
+    if 'http' != link[:4]:
+        return False
+    if Y_LINK in link:
+        return False
+    return True
+
+
+async def get_external_link_from_page(session, url):
+    try:
+        page = await session.get(url, timeout=REQUEST_TIMEOUT)
+        page = await page.text()
+        soup = BeautifulSoup(page, "html.parser")
+        all_links = map(lambda anchor: anchor['href'], soup.findAll('a'))
+        return list(filter(filter_link, all_links))
+    except Exception as e:
+        return []
+
+
+def create_dir(abs_path):
+    if not os.path.exists(abs_path):
+        os.makedirs(abs_path)
+
+
+async def save_url_to_disk(session, url, abs_path):
+    content = await get_content(session, url) or 'No response from server'
+    await write_to_file(abs_path, content)
+
+
+async def worker(session):
+    start_time = time.time()
+    print("start time: ", start_time)
+    all_news = await find_news_blocks(session, URL)
+    for news in all_news[:2]:
         news_link = news['news_link']
         if news_link:
             dir_name = generate_name_from_link(news_link)
-            content = get_content(news_link)
-            create_dir(dir_name)
-            filename = DOWNLOAD_DIR + dir_name + dir_name + '.html'
-            write_to_file(filename, content)
+            news_dir_name = DOWNLOAD_DIR + dir_name
+            create_dir(news_dir_name)
+            news_filename = news_dir_name + dir_name + '.html'
+            await save_url_to_disk(session, news_link, news_filename)
+
+            comment = news['comment_link']
+            if comment:
+                comment_links = await get_external_link_from_page(session, comment)
+                for link in comment_links[:3]:
+                    comment_dir_name = news_dir_name + '/comments'
+                    create_dir(comment_dir_name)
+                    comment_filename = comment_dir_name + generate_name_from_link(link) + '.html'
+                    await save_url_to_disk(session, link, comment_filename)
+
+    end_time = time.time()
+    print("finished at: ", end_time)
+    print("duration: ", end_time - start_time)
+
+
+async def main():
+    async with aiohttp.ClientSession() as session:
+        while True:
+            await worker(session)
+            await asyncio.sleep(CYCLE_SECONDS)
 
 
 if __name__ == '__main__':
-    main()
+    asyncio.run(main())
+
